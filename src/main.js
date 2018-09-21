3d8b2f9237100ae9574e519c8793537c13d8aaa6
bedakb <belmoo996@hotmail.com>
2018-09-21 10:22:30 +0200

    add support for tags

--- src/main.js
+++ src/main.js
@@ -1,22 +1,33 @@
 const KEYWORDS_URL = chrome.runtime.getURL('keywords.txt')
 const CONTAINER = document.querySelector('.block-news') || document
 
-const findAnnoyingPosts = (post, keywords) => {
-    let title = post.querySelector('h1').textContent
+const findKeywords = (post, lookupText, keywords) => {
     return keywords.some(keyword => {
         let regex = new RegExp(keyword, 'gi')
-        return title.match(regex)
+        return lookupText.match(regex)
     })
 }
 
-const removeAnnoyingPosts = keywords => {
+const removePosts = keywords => {
     let posts = [...document.querySelectorAll('.kolona')]
     keywords = keywords.split('\n')
 
     posts.forEach(post => {
-        let found = findAnnoyingPosts(post, keywords)
+        let found = findKeywords(post, post.querySelector('h1').textContent, keywords)
         if (found) {
             post.style.display = 'none'
+        }
+    })
+}
+
+const removeTags = keywords => {
+    let tags = [...document.querySelectorAll('.tagovi a')]
+    keywords = keywords.split('\n')
+
+    tags.forEach(tag => {
+        let found = findKeywords(tag, tag.textContent, keywords)
+        if (found) {
+            tag.style.display = 'none'
         }
     })
 }
@@ -27,11 +38,12 @@ chrome.runtime.sendMessage({}, response => {
         .then(response => {
 
             // Initially remove all annoying posts
-            removeAnnoyingPosts(response)
+            removePosts(response)
+            removeTags(response)
 
             // In case change in DOM happend perform remove action again
             let observer = new MutationObserver(mutations => {
-                removeAnnoyingPosts(response)
+                removePosts(response)
             })
             observer.observe(CONTAINER, { childList: true })
 
